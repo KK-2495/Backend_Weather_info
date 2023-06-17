1ffe78f9a7ef14c64b1f29641ae59bb4bee3002d
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-06-17 12:49:10 +0530

    update

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ mongoose.connect('mongodb+srv://Krish24:Krish%402495@cluster0.s8xz5ha.mongodb.ne
 app.listen(4000, ()=>console.log("connected on port"));
 
 
+// let a = [1,1,1,2,2,3,3,3,3,3,4,4,5,5,5,5,5,6];
+// let dup = [];
+// let uniq = [];
+// for(let i=0; i<a.length; i++){
+//     if(a[i] == a[i+1]){
+//         // dup.push(a[i]);
+//         a.splice(a[i],0,a[i]);
+//     }
+// }
+// console.log(a);
+
